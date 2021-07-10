c685ade1707fdfa5d97761b8125e9687adb71f49
Felo Vilches <felovilches@gmail.com>
2021-07-10 14:12:03 +0900

    first

---
+++ spec/lib/tree_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require_relative '../loader'
+
+describe Tree do
+  describe 'init' do
+    context 'init' do
+      it { expect(2).to eq 2 }
+    end
+  end
+end
---
+++ spec/loader.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+# Load all files from lib.
+Dir[File.join(__dir__, '../', 'lib', '*.rb')].sort.each { |file| require file }
