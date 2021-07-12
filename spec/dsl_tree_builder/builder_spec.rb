d5f0b24fe5dee84b7bcf266680ffcbe2048aa2a2
Felo Vilches <felovilches@gmail.com>
2021-07-12 21:34:12 +0900

    add unused spec

--- spec/dsl_tree_builder/builder_spec.rb
+++ spec/dsl_tree_builder/builder_spec.rb
@@ -11,4 +11,31 @@ describe BehaviorTree::Builder do
       end
     end
   end
+
+  # NOTE: Specs below are for per-instance registration (not implemented yet).
+  #       These specs are fully working. But it's also necessary to test tree creation
+  #       using .new (instance builder). Currently all specs are using the static method.
+  #
+  # describe '.register' do
+  #   context 'new instance' do
+  #     let(:instance) { described_class.new }
+  #     let(:static_mapping) { described_class.instance_variable_get(:@node_type_mapping) }
+  #     let(:instance_mapping) { instance.instance_variable_get(:@node_type_mapping) }
+  #
+  #     before { instance.register(:instance1_only, 'FakeClass', children: :multiple) }
+  #     it { expect(static_mapping).to_not have_key :instance1_only }
+  #     it { expect(instance_mapping).to have_key :instance1_only }
+  #
+  #     context 'yet another instance created' do
+  #       let(:another_instance) { described_class.new }
+  #       let(:another_instance_mapping) { another_instance.instance_variable_get(:@node_type_mapping) }
+  #       before { another_instance.register(:instance2_only, 'SomeClass', children: :single) }
+  #
+  #       it { expect(static_mapping).to_not have_key :instance2_only }
+  #       it { expect(instance_mapping).to_not have_key :instance2_only }
+  #       it { expect(another_instance_mapping).to have_key :instance2_only }
+  #       it { expect(another_instance_mapping).to_not have_key :instance1_only }
+  #     end
+  #   end
+  # end
 end
