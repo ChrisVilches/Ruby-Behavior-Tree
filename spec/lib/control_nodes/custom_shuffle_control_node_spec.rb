c1586933a2adfa5c317022002ce8af865f65a786
Felo Vilches <felovilches@gmail.com>
2021-07-13 23:09:54 +0900

    shuffle spec (wip)

--- spec/lib/control_nodes/custom_shuffle_control_node_spec.rb
+++ spec/lib/control_nodes/custom_shuffle_control_node_spec.rb
@@ -1,5 +1,26 @@
 # frozen_string_literal: true
 
-describe Array do
+module TestControlNodes
+  class Shuffle < BehaviorTree::Selector
+    children_traversal_strategy :shuffle
+
+    private
+
+    # Memoize shuffled order. Keep the same order while the selector is running.
+    def shuffle
+      @shuffle ||= @children.shuffle
+    end
+
+    # Un-memoize the shuffled order so that it's shuffled again (everytime the status goes from
+    # not-running to running).
+    def on_started_running
+      @shuffle = nil
+    end
+  end
+end
+
+describe TestControlNodes::Shuffle do
   pending 'Make a custom control node for shuffling and memoizing the order until next running! state set.'
+  pending 'shuffle order is the same while it is running'
+  pending 'shuffle order changes when the node re-enters running state (coming from not-running)'
 end
