4f718d8cce16654f5439b7accdd87075c7ff6aef
Felo Vilches <felovilches@gmail.com>
2021-07-15 11:30:06 +0900

    would you please let me compile this? thanks

--- lib/behavior_tree.rb
+++ lib/behavior_tree.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 # Load all files from lib.
-Gem.find_files('behavior_tree/**/*.rb').each { |path| require path }
+# Gem.find_files('behavior_tree/**/*.rb').each { |path| require path }
+
+Dir.glob(File.join('./lib', '**', '*.rb')).sort.each { |file| require file }
